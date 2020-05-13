2764b95a467b65e9891cded3ace6bb0cee5ee1f0
Grayson Deese <gldeese@ncsu.edu>
2020-05-13 19:50:40 -0400

    Properly displaying the selected city and date.

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 var weatherCurrent = document.querySelector(".weatherCurrent");
 var fiveDay = document.getElementById("fiveDay");
 var searches = JSON.parse(localStorage.getItem("searches")) || [];
+// current time vars
+var today = new Date();
+var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
 // Showing the search history      
 function renderHistory() {
         $("#history").empty();
@@ -29,16 +32,15 @@ function renderHistory() {
 function displayWeatherInfo(city){
   // setting up the query URL
    // need to make this input what the enter in the city search box
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=dadc20b0e9f1fa15e91b1c1cf640bb06" 
-  ;
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=dadc20b0e9f1fa15e91b1c1cf640bb06" + "&units=imperial";
   // Creating the AJAX call 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
 console.log(response);
-// creating div to hold response
-
+// displaying the name of the city
+$(weatherCurrent).html("<h1>" + response.name + ": " + "("+ date + ")"+ "</h1>");
 
   });
 }
